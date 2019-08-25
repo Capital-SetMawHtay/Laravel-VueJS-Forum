1372a48e1da60dcfe948588aacaf9bed4215eec5
unknown <firasjerbiv2@gmail.com>
2019-08-25 22:04:40 +0100

    Models and routes

---
+++ app/Model/Question.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        $this->hasMany(Reply::class);
+    }
+
+    public function category(){
+return $this->belongsTo(Category::class);
+    }
+}
