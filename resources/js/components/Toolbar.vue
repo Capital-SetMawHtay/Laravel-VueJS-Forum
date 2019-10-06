c8a842855bf4b230f5a36a05d034bc733e00d5d6
fjerbi <firasjerbiv2@gmail.com>
2019-10-06 20:12:47 +0100

    vue login finished

--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -3,12 +3,16 @@
 <v-toolbar-title>Hkeyti</v-toolbar-title>
 <v-spacer></v-spacer>
 
-<v-toolbar-items class="hidden-sm-and-down">
+<div class="hidden-sm-and-down">
+
 <v-btn flat>Forum</v-btn>
 <v-btn flat>Ask Question</v-btn>
 <v-btn flat>Category</v-btn>
+  <router-link to="/login">
 <v-btn flat>Login</v-btn>
-</v-toolbar-items>
+    </router-link>
+
+</div>
   </v-toolbar>
 </template>
 <script>
