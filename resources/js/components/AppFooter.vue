0b15bb2078076ecc707fcf5492e8f7978d2d2b05
fjerbi <firasjerbiv2@gmail.com>
2019-10-06 19:29:50 +0100

    vuejs routing

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer>
+      Hkeyti
+    <div class="flex-grow-1"></div>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
